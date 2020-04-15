2ae293e8b33d57be7681f94a989e96c769b77dce
shazmalik <shahzad.cloudtek@gmail.com>
2020-04-15 15:30:25 -0700

    handlebar added

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function loadData() {
   $.get("https://aimtell.com/files/sites.json", function (data, status) {
-    console.log(data);
-    $("button").html("Next Step...");
+    let previewData = data.sites.map(s => {
+      return { id: s.id, name: s.name, url: s.url };
+    });
+    console.log(previewData);
+
+    // Get the text for the Handlebars template from the script element.
+    let templateText = $("#tableTemplate").html();
+
+    // Compile the Handlebars template.
+    let tableTemplate = Handlebars.compile(templateText);
+    $("#data").html(tableTemplate({ array: previewData }));
+
+    $("button").html("Data Loaded");
   });
 }
